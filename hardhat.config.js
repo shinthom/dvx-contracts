6eda66287d0ad9633b9cb20113f292ee92539def
0xdagarn <sgnoo@naver.com>
2023-12-14 10:26:17 +0900

    chore: add tasks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,108 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("mine", "Mine a block").setAction(async (_args, hre) => {
+  await hre.network.provider.request({
+    method: "evm_mine",
+  });
+});
+
+task("unset-automine", "Unset automine").setAction(async (_args, hre) => {
+  await hre.network.provider.request({
+    method: "evm_setAutomine",
+    params: [false],
+  });
+});
+
+// hardhat execute-order:increase --account 0x988aa44e12c7bce07e449a4156b4a269d6642b3a --network local
+task("execute-order:increase", "Execute GMX V1 increase order")
+  .addParam("account", "Account address")
+  .setAction(async ({ account }, { ethers }) => {
+    const PositionRouter = "0xb87a436b93ffe9d75c5cfa7bacfff96430b09868";
+    const positionRouter = await ethers.getContractAt(
+      "IPositionRouter",
+      PositionRouter
+    );
+
+    const increasePositionsIndex = await positionRouter.increasePositionsIndex(
+      account
+    );
+    const requestKey = await positionRouter.getRequestKey(
+      account,
+      increasePositionsIndex
+    );
+
+    const impersonatedPositionKeeper = await ethers.getImpersonatedSigner(
+      "0xDd763ED8Ce604E9a61F1e1aed433c1362e05700d"
+    );
+    await positionRouter
+      .connect(impersonatedPositionKeeper)
+      .executeIncreasePosition(requestKey, user0.address);
+  });
+
+// hardhat execute-order:decrease --account 0x988aa44e12c7bce07e449a4156b4a269d6642b3a --network local
+task("execute-order:decrease", "Execute GMX V1 decrease order")
+  .addParam("account", "Account address")
+  .setAction(async ({ account }, { ethers }) => {
+    const PositionRouter = "0xb87a436b93ffe9d75c5cfa7bacfff96430b09868";
+    const positionRouter = await ethers.getContractAt(
+      "IPositionRouter",
+      PositionRouter
+    );
+
+    const decreasePositionsIndex = await positionRouter.decreasePositionsIndex(
+      account
+    );
+    const requestKey = await positionRouter.getRequestKey(
+      account,
+      decreasePositionsIndex
+    );
+
+    const impersonatedPositionKeeper = await ethers.getImpersonatedSigner(
+      "0xDd763ED8Ce604E9a61F1e1aed433c1362e05700d"
+    );
+    await positionRouter
+      .connect(impersonatedPositionKeeper)
+      .executeDecreasePosition(requestKey, user0.address);
+  });
+
+// hardhat fill-order:increase --network local
+task("fill-order:increase", "Execute MUX order").setAction(
+  async (_, { ethers }) => {
+    const OrderBook = "0xa19fD5aB6C8DCffa2A295F78a5Bb4aC543AAF5e3";
+    const orderBook = await ethers.getContractAt("IOrderBook", OrderBook);
+    const orderId = (await orderBook.nextOrderId()) - 1n;
+
+    const impersonatedBroker = await ethers.getImpersonatedSigner(
+      "0x988aa44e12c7bce07e449a4156b4a269d6642b3a" // mux broker
+    );
+    await orderBook.connect(impersonatedBroker).fillPositionOrder(
+      orderId,
+      1, // collateralPrice
+      1, // assetPrice
+      1 // profitAssetPrice
+    );
+  }
+);
+
+// hardhat fill-order:decrease --network local
+task("fill-order:decrease", "Execute MUX order").setAction(
+  async (_, { ethers }) => {
+    const OrderBook = "0xa19fD5aB6C8DCffa2A295F78a5Bb4aC543AAF5e3";
+    const orderBook = await ethers.getContractAt("IOrderBook", OrderBook);
+    const orderId = (await orderBook.nextOrderId()) - 1n;
+
+    const impersonatedBroker = await ethers.getImpersonatedSigner(
+      "0x988aa44e12c7bce07e449a4156b4a269d6642b3a" // mux broker
+    );
+    await orderBook.connect(impersonatedBroker).fillWithdrawalOrder(
+      orderId,
+      1, // collateralPrice
+      1, // assetPrice
+      1 // profitAssetPrice
+    );
+  }
+);
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
