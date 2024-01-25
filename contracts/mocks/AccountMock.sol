d3eeb15c6aae990cc3f081809ac571c5c9b47178
0xdagarn <sgnoo@naver.com>
2024-01-25 11:50:45 +0900

    refactor: update Mocks

--- contracts/mocks/AccountMock.sol
+++ contracts/mocks/AccountMock.sol
@@ -1,40 +1,40 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.7;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity 0.8.7;
 
-import {Account} from "../Account.sol";
-import {IExchange} from "../interfaces/IExchange.sol";
-import "hardhat/console.sol";
+// import {Account} from "../Account.sol";
+// import {IExchange} from "../interfaces/IExchange.sol";
+// import "hardhat/console.sol";
 
-contract AccountMock is Account {
-    uint256 private _balance;
+// contract AccountMock is Account {
+//     uint256 private _balance;
 
-    constructor(address _owner, address _exchange) Account(_owner, _exchange) {}
+//     constructor(address _owner, address _exchange) Account(_owner, _exchange) {}
 
-    function getBalance(address token) public view override returns (uint256) {
-        return _balance;
-    }
+//     function getBalance(address token) public view override returns (uint256) {
+//         return _balance;
+//     }
 
-    function setBalance(uint256 balance) external {
-        _balance = balance;
-    }
+//     function setBalance(uint256 balance) external {
+//         _balance = balance;
+//     }
 
-    function increasePosition(
-        address adapter,
-        IExchange.MarketOrder calldata marketOrder
-    ) external payable override {}
+//     function increasePosition(
+//         address adapter,
+//         IExchange.MarketOrder calldata marketOrder
+//     ) external payable override {}
 
-    function decreasePosition(
-        address adapter,
-        IExchange.MarketOrder calldata marketOrder
-    ) external payable override {}
+//     function decreasePosition(
+//         address adapter,
+//         IExchange.MarketOrder calldata marketOrder
+//     ) external payable override {}
 
-    function increaseCollateral(
-        address adapter,
-        IExchange.MarketOrder calldata marketOrder
-    ) external payable override {}
+//     function increaseCollateral(
+//         address adapter,
+//         IExchange.MarketOrder calldata marketOrder
+//     ) external payable override {}
 
-    function decreaseCollateral(
-        address adapter,
-        IExchange.MarketOrder calldata marketOrder
-    ) external payable override {}
-}
+//     function decreaseCollateral(
+//         address adapter,
+//         IExchange.MarketOrder calldata marketOrder
+//     ) external payable override {}
+// }
--- contracts/mocks/ExchangeMock.sol
+++ contracts/mocks/ExchangeMock.sol
@@ -1,47 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.7;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity 0.8.7;
 
-import {Exchange} from "../Exchange.sol";
-import {AccountMock} from "./AccountMock.sol";
+// import {Exchange} from "../Exchange.sol";
+// import {AccountMock} from "./AccountMock.sol";
 
-contract ExchangeMock is Exchange {
-    uint256 private _lockedBalance;
+// contract ExchangeMock is Exchange {
+//     uint256 private _lockedBalance;
 
-    function increaseLockedBalance(uint256 amount) external {
-        _lockedBalance = amount;
-    }
+//     function increaseLockedBalance(uint256 amount) external {
+//         _lockedBalance = amount;
+//     }
 
-    function lockedBalances(
-        address account,
-        address token
-    ) external view override returns (uint256) {
-        return _lockedBalance;
-    }
+//     function lockedBalances(
+//         address account,
+//         address token
+//     ) external view override returns (uint256) {
+//         return _lockedBalance;
+//     }
 
-    function swap(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn
-    ) public payable override returns (uint256 amountOut) {
-        amountOut = 10;
-    }
+//     function swap(
+//         address tokenIn,
+//         address tokenOut,
+//         uint256 amountIn
+//     ) public payable override returns (uint256 amountOut) {
+//         amountOut = 10;
+//     }
 
-    function createAccountMock() external returns (address) {
-        AccountMock account = new AccountMock(msg.sender, address(this));
-        accounts[msg.sender] = address(account);
+//     function createAccountMock() external returns (address) {
+//         AccountMock account = new AccountMock(msg.sender, address(this));
+//         accounts[msg.sender] = address(account);
 
-        return address(account);
-    }
+//         return address(account);
+//     }
 
-    function executeLimitOrder(
-        address account,
-        address adapter,
-        MarketOrder memory marketOrder
-    ) external payable override {}
+//     function executeLimitOrder(
+//         address account,
+//         address adapter,
+//         MarketOrder memory marketOrder
+//     ) external payable override {}
 
-    function executeTriggerOrder(
-        address account,
-        address adapter,
-        MarketOrder memory marketOrder
-    ) external payable override {}
-}
+//     function executeTriggerOrder(
+//         address account,
+//         address adapter,
+//         MarketOrder memory marketOrder
+//     ) external payable override {}
+// }
