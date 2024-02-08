57f4da568994ae6a10fb5f33b3ae643b4b60d9fd
0xdagarn <sgnoo@naver.com>
2024-02-08 17:55:57 +0900

    test: relay limit order

--- test/e2e/limitOrder.js
+++ test/e2e/limitOrder.js
@@ -3,19 +3,16 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { deploy } = require("../fixture");
 
 describe("limitOrder", () => {
-  it("runs", async () => {
+  it("gmx v1: directly from EOA", async () => {
     const {
       owner,
       orderKeeper,
       account,
       warehouse,
       gmxV1Adapter,
-      muxAdapter,
       WETH,
-      checkBalance,
       deposit,
       executeIncreasePosition,
-      fillPositionOrder,
       setPrice,
     } = await loadFixture(deploy);
 
@@ -29,147 +26,385 @@ describe("limitOrder", () => {
     await setPrice(gmxV1Adapter, WETH, price, price, false);
 
     var triggerPrice = ethers.parseUnits("2000", 18);
-    var acceptablePrice = ethers.parseUnits("2000", 18); // calculated by slippage tolerance
+    var acceptablePrice = ethers.parseUnits("2000", 18);
 
     await deposit(collateral, collateralAmount);
-    await checkBalance(account);
 
     await account.connect(owner).createLimitOrder(
       collateral,
       index,
-      collateralAmount / 2n,
+      collateralAmount,
       size,
       isLong,
-      0, // execution fee
       triggerPrice,
-      acceptablePrice
+      acceptablePrice,
+      0, // execution fee,
+      "0x"
     );
     console.log(await warehouse.getLimitOrders(account.target));
 
     const limitOrder = await warehouse.getLimitOrder(account.target, 0);
     console.log(limitOrder.limitOrderId);
-    await account.connect(owner).cancelLimitOrder(
-      limitOrder.limitOrderId,
-      0 // execution fee
+    await account
+      .connect(owner)
+      .cancelLimitOrder(limitOrder.limitOrderId, 0, "0x");
+
+    await account.connect(owner).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      0, // execution fee,
+      "0x"
     );
+    console.log(await warehouse.getLimitOrders(account.target));
 
-    //   await account.connect(owner).createLimitOrder(
-    //     collateral,
-    //     index,
-    //     collateralAmount,
-    //     size,
-    //     isLong,
-    //     0, // execution fee
-    //     triggerPrice,
-    //     acceptablePrice
-    //   );
+    var adapterFee = await gmxV1Adapter.getMinExecutionFee();
+    await account
+      .connect(orderKeeper)
+      .executeLimitOrder(1, gmxV1Adapter.target, {
+        value: adapterFee,
+      });
+    await executeIncreasePosition(account.target);
+    console.log(
+      await gmxV1Adapter.getPosition(account.target, collateral, index, isLong)
+    );
+  });
 
-    //   var executionFee = await gmxV1Adapter.getMinExecutionFee();
-    //   await account
-    //     .connect(orderKeeper)
-    //     .executeLimitOrder(1, gmxV1Adapter.target, 0, {
-    //       value: executionFee,
-    //     });
-    //   await executeIncreasePosition(account.target);
-    //   console.log(
-    //     await gmxV1Adapter.getPosition(account.target, collateral, index, isLong)
-    //   );
+  it("gmx v1: relayed from relayer", async () => {
+    const {
+      va,
+      owner,
+      relayer,
+      orderKeeper,
+      account,
+      warehouse,
+      gmxV1Adapter,
+      WETH,
+      deposit,
+      executeIncreasePosition,
+      setPrice,
+    } = await loadFixture(deploy);
 
-    //   var ethPrice = ethers.parseUnits("2000", 8);
-    //   await setPrice(muxAdapter, WETH, ethPrice, ethPrice, false);
+    const collateral = WETH;
+    const index = WETH;
+    const collateralAmount = ethers.parseEther("1");
+    const size = ethers.parseEther("10");
+    const isLong = true;
 
-    //   await deposit(collateral, collateralAmount);
-    //   await account
-    //     .connect(owner)
-    //     .createLimitOrder(
-    //       collateral,
-    //       index,
-    //       collateralAmount,
-    //       size,
-    //       isLong,
-    //       executionFee,
-    //       triggerPrice,
-    //       acceptablePrice
-    //     );
-    //   await account
-    //     .connect(orderKeeper)
-    //     .executeLimitOrder(2, muxAdapter.target, 0, {
-    //       value: 0,
-    //     });
-    //   await fillPositionOrder();
-    //   console.log(
-    //     await muxAdapter.getPosition(account.target, collateral, index, isLong)
-    //   );
-    // });
+    var price = ethers.parseUnits("2000", 30);
+    await setPrice(gmxV1Adapter, WETH, price, price, false);
 
-    // it("multi", async () => {
-    //   const {
-    //     owner,
-    //     orderKeeper,
-    //     account,
-    //     warehouse,
-    //     gmxV1Adapter,
-    //     muxAdapter,
-    //     WETH,
-    //     deposit,
-    //     executeIncreasePosition,
-    //     fillPositionOrder,
-    //     setDummyPrice,
-    //   } = await loadFixture(deploy);
+    var executionFee = 0;
+    var triggerPrice = ethers.parseUnits("2000", 18);
+    var acceptablePrice = ethers.parseUnits("2000", 18);
 
-    //   const collateral = WETH;
-    //   const index = WETH;
-    //   const collateralAmount = ethers.parseEther("1");
-    //   const size = ethers.parseEther("10");
-    //   const isLong = true;
+    await deposit(collateral, collateralAmount);
 
-    //   await setDummyPrice();
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "address", // collateral
+        "address", // index
+        "uint256", // collateralAmount
+        "uint256", // size
+        "bool", // isLong
+        "uint256", // triggerPrice
+        "uint256", // acceptablePrice
+        "uint256", // executionFee
+      ],
+      [
+        collateral,
+        index,
+        collateralAmount,
+        size,
+        isLong,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+      ]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
 
-    //   var triggerPrice = ethers.parseUnits("2000", 18);
-    //   var acceptablePrice = ethers.parseUnits("2000", 18); // calculated by slippage tolerance
+    await account.connect(relayer).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      executionFee, // execution fee,
+      signature
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
 
-    //   await deposit(collateral, collateralAmount);
-    //   await account.connect(owner).createLimitOrder(
-    //     collateral,
-    //     index,
-    //     collateralAmount,
-    //     size,
-    //     isLong,
-    //     0, // execution fee
-    //     triggerPrice,
-    //     acceptablePrice
-    //   );
-    //   console.log(await warehouse.getLimitOrders(account.target));
+    const limitOrder = await warehouse.getLimitOrder(account.target, 0);
 
-    //   var adapterFee = await gmxV1Adapter.getMinExecutionFee();
-    //   await account
-    //     .connect(orderKeeper)
-    //     .executeLimitOrderMulti(
-    //       0,
-    //       [gmxV1Adapter.target, muxAdapter.target],
-    //       [collateralAmount / 2n, collateralAmount / 2n],
-    //       [size / 2n, size / 2n],
-    //       0,
-    //       { value: adapterFee }
-    //     );
-    //   await executeIncreasePosition(account.target);
-    //   await fillPositionOrder();
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "uint256", // limitOrderId
+        "uint256", // executionFee
+      ],
+      [limitOrder.limitOrderId, executionFee]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+    await account
+      .connect(owner)
+      .cancelLimitOrder(limitOrder.limitOrderId, executionFee, signature);
 
-    //   console.log(
-    //     await gmxV1Adapter.getWrapPosition(
-    //       account.target,
-    //       collateral,
-    //       index,
-    //       isLong
-    //     )
-    //   );
-    //   console.log(
-    //     await muxAdapter.getWrapPosition(
-    //       account.target,
-    //       collateral,
-    //       index,
-    //       isLong
-    //     )
-    //   );
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "address", // collateral
+        "address", // index
+        "uint256", // collateralAmount
+        "uint256", // size
+        "bool", // isLong
+        "uint256", // triggerPrice
+        "uint256", // acceptablePrice
+        "uint256", // executionFee
+      ],
+      [
+        collateral,
+        index,
+        collateralAmount,
+        size,
+        isLong,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+      ]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+
+    await account.connect(relayer).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      executionFee, // execution fee,
+      signature
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
+
+    var adapterFee = await gmxV1Adapter.getMinExecutionFee();
+    await account
+      .connect(orderKeeper)
+      .executeLimitOrder(1, gmxV1Adapter.target, {
+        value: adapterFee,
+      });
+    await executeIncreasePosition(account.target);
+    console.log(
+      await gmxV1Adapter.getPosition(account.target, collateral, index, isLong)
+    );
+  });
+
+  it("mux: directly from EOA", async () => {
+    const {
+      owner,
+      orderKeeper,
+      account,
+      warehouse,
+      gmxV1Adapter,
+      muxAdapter,
+      WETH,
+      deposit,
+      fillPositionOrder,
+      setPrice,
+    } = await loadFixture(deploy);
+
+    const collateral = WETH;
+    const index = WETH;
+    const collateralAmount = ethers.parseEther("1");
+    const size = ethers.parseEther("10");
+    const isLong = true;
+
+    var price = ethers.parseUnits("2000", 8);
+    await setPrice(gmxV1Adapter, WETH, price, price, false);
+
+    var triggerPrice = ethers.parseUnits("2000", 18);
+    var acceptablePrice = ethers.parseUnits("2000", 18);
+
+    await deposit(collateral, collateralAmount);
+
+    await account.connect(owner).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      0, // execution fee,
+      "0x"
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
+
+    const limitOrder = await warehouse.getLimitOrder(account.target, 0);
+    console.log(limitOrder.limitOrderId);
+    await account
+      .connect(owner)
+      .cancelLimitOrder(limitOrder.limitOrderId, 0, "0x");
+
+    await account.connect(owner).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      0, // execution fee,
+      "0x"
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
+
+    var adapterFee = await muxAdapter.getMinExecutionFee();
+    await account.connect(orderKeeper).executeLimitOrder(1, muxAdapter.target, {
+      value: adapterFee,
+    });
+    await fillPositionOrder();
+    console.log(
+      await muxAdapter.getPosition(account.target, collateral, index, isLong)
+    );
+  });
+
+  it("mux: relayed from relayer", async () => {
+    const {
+      va,
+      owner,
+      relayer,
+      orderKeeper,
+      account,
+      warehouse,
+      muxAdapter,
+      WETH,
+      deposit,
+      fillPositionOrder,
+      setPrice,
+    } = await loadFixture(deploy);
+
+    const collateral = WETH;
+    const index = WETH;
+    const collateralAmount = ethers.parseEther("1");
+    const size = ethers.parseEther("10");
+    const isLong = true;
+
+    var price = ethers.parseUnits("2000", 8);
+    await setPrice(muxAdapter, WETH, price, price, false);
+
+    var executionFee = 0;
+    var triggerPrice = ethers.parseUnits("2000", 18);
+    var acceptablePrice = ethers.parseUnits("2000", 18);
+
+    await deposit(collateral, collateralAmount);
+
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "address", // collateral
+        "address", // index
+        "uint256", // collateralAmount
+        "uint256", // size
+        "bool", // isLong
+        "uint256", // triggerPrice
+        "uint256", // acceptablePrice
+        "uint256", // executionFee
+      ],
+      [
+        collateral,
+        index,
+        collateralAmount,
+        size,
+        isLong,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+      ]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+
+    await account.connect(relayer).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      executionFee, // execution fee,
+      signature
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
+
+    const limitOrder = await warehouse.getLimitOrder(account.target, 0);
+
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "uint256", // limitOrderId
+        "uint256", // executionFee
+      ],
+      [limitOrder.limitOrderId, executionFee]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+    await account
+      .connect(owner)
+      .cancelLimitOrder(limitOrder.limitOrderId, executionFee, signature);
+
+    // make signature from relayer
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "address", // collateral
+        "address", // index
+        "uint256", // collateralAmount
+        "uint256", // size
+        "bool", // isLong
+        "uint256", // triggerPrice
+        "uint256", // acceptablePrice
+        "uint256", // executionFee
+      ],
+      [
+        collateral,
+        index,
+        collateralAmount,
+        size,
+        isLong,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+      ]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+
+    await account.connect(relayer).createLimitOrder(
+      collateral,
+      index,
+      collateralAmount,
+      size,
+      isLong,
+      triggerPrice,
+      acceptablePrice,
+      executionFee, // execution fee,
+      signature
+    );
+    console.log(await warehouse.getLimitOrders(account.target));
+
+    var adapterFee = await muxAdapter.getMinExecutionFee();
+    await account.connect(orderKeeper).executeLimitOrder(1, muxAdapter.target, {
+      value: adapterFee,
+    });
+    await fillPositionOrder();
+    console.log(
+      await muxAdapter.getPosition(account.target, collateral, index, isLong)
+    );
   });
 });
