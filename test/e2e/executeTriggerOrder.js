253e977eabcf116d549f99fb1775e60c48e98a20
0xdagarn <sgnoo@naver.com>
2024-02-09 11:36:15 +0900

    test: add executeTriggerOrder for mux

--- test/e2e/executeTriggerOrder.js
+++ test/e2e/executeTriggerOrder.js
@@ -85,4 +85,80 @@ describe("executeTriggerOrder", () => {
     await executeDecreasePosition(account.target);
     await checkPosition(gmxV1Adapter, account, collateral, index, isLong);
   });
+
+  it("mux", async () => {
+    const {
+      va,
+      account,
+      orderKeeper,
+      muxAdapter,
+      WETH,
+      checkPosition,
+      setDummyPrice,
+      deposit,
+      increasePosition,
+      fillPositionOrder,
+    } = await loadFixture(deploy);
+    await setDummyPrice();
+
+    var collateral = WETH;
+    var index = WETH;
+    var collateralAmount = ethers.parseEther("1");
+    var size = ethers.parseEther("10");
+    var isLong = true;
+    await deposit(collateral, collateralAmount, "0x");
+    await increasePosition(muxAdapter, collateral, index, collateralAmount, size, acceptablePrice, isLong); // prettier-ignore
+    await checkPosition(muxAdapter, account, collateral, index, isLong);
+
+    var executionFee = 0;
+    var triggerPrice = ethers.parseUnits("2000", 18);
+    var acceptablePrice = ethers.parseUnits("2000", 18);
+
+    var messageHash = ethers.solidityPackedKeccak256(
+      [
+        "address", // adapter
+        "address", // collateral
+        "address", // index
+        "bool", // isLong
+        "uint256", // size
+        "uint8", // orderType
+        "uint256", // triggerPrice
+        "uint256", // acceptablePrice
+        "uint256", // executionFee
+        "uint256", // deadline
+      ],
+      [
+        muxAdapter.target,
+        collateral,
+        index,
+        isLong,
+        size,
+        orderType.tp,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+        deadline,
+      ]
+    );
+    var signature = await va.signMessage(ethers.getBytes(messageHash));
+    var adapterFee = await muxAdapter.getMinExecutionFee();
+    await account
+      .connect(orderKeeper)
+      .executeTriggerOrder(
+        muxAdapter.target,
+        collateral,
+        index,
+        isLong,
+        size,
+        orderType.tp,
+        triggerPrice,
+        acceptablePrice,
+        executionFee,
+        deadline,
+        signature,
+        { value: adapterFee }
+      );
+    await fillPositionOrder();
+    await checkPosition(muxAdapter, account, collateral, index, isLong);
+  });
 });
